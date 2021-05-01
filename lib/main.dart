6fb42214fe24784dd4e5d3589b0177adeb5c2739
KamilleJS <jkuklinaj@gmail.com>
2021-05-01 15:19:55 -0700

    gold

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.amber,
-          appBar: AppBar(
-            title: Center(
-              child: Text('I Am Gold'),
-            ),
-            backgroundColor: Colors.orangeAccent,
-          )),
+        backgroundColor: Colors.amber,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Gold'),
+          ),
+          backgroundColor: Colors.orangeAccent,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/gg.png'),
+          ),
+        ),
+      ),
     ),
   );
 }
